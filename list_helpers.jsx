0b7789b290b0a58acae230e9f1ba0e7a6688b9fa
sach <sacha.saint-leger@mail.mcgill.ca>
2015-12-27 14:10:29 -0500

    showed how to mimic sum using reduce1 (called it sum1)

--- list_helpers.jsx
+++ list_helpers.jsx
@@ -1,22 +1,144 @@
-//To do: undestructive unshift and wrap function in another that hides acc
 
-var map = function (items, transform) {
+//To do: undestructive unshift
 
+var map = function (transform, items) {
+
+    if (typeof items == "undefined") { //partial
+	return function (items) {
+	    var acc = [];
+	    if ( items.length == 0) { return [] ;}
+	    function aux (items, transform, acc) {
+		var [h, ...t] = items;
+
+         	if (t.length == 0) {
+		    acc.push(transform(h));
+	 	    return acc; }
+	        else {
+		    acc.push(transform(h));
+		    return aux(t, transform, acc);
+		}
+	    }
+	    return aux (items, transform, acc);
+	};
+    }
+    // full
     var acc = [];
-    if ( items.length == 0) { return []}
+    if ( items.length == 0) { return [] ;}
 
     function aux (items, transform, acc) {
+
 	var [h, ...t] = items;
+
 	if (t.length == 0) {
-	    acc.unshift(transform(h));
+	    acc.push(transform(h));
 	    return acc; }
 	else { 
-	    acc.unshift(transform(h));
+	    acc.push(transform(h));
 	    return aux(t, transform, acc);
 	}
     }
 
     return aux (items, transform, acc);
-}
+};
+// map (x => x + 1, [1,2,3]);
+// var add_one = map(x => x + 1);
+// add_one([1,2,3]);
 
-//  map ([1,2], x => x + 1);;
+var filter = function (pred, list) {
+    if (typeof list == "undefined") {
+	return function(list) { //partial
+	    var acc = [];
+	    if ( list.length == 0) { return []; }
+	    function aux (list, pred, acc) {
+		var [h, ...t] = list;
+		if (t.length == 0) {
+		    if (pred(h)) {acc.push(h);}
+		    return acc;
+		}
+		else {
+		    if (pred(h)) {acc.push(h);}
+		    return aux(t, pred, acc);
+		}
+	    }
+	    return aux (list, pred, acc);
+	};
+    }
+    // full
+    var acc = [];
+    if ( list.length == 0) { return []; }
+    function aux (list, pred, acc) {
+	var [h, ...t] = list;
+	if (t.length == 0) {
+	    if (pred(h)) {acc.push(h);}
+	    return acc;
+	}
+	else {
+	    if (pred(h)) {acc.push(h);}
+	    return aux(t, pred, acc);
+	}
+    }
+    return aux (list, pred, acc);
+};
+    
+// filter(x => ( x % 2 == 0), [1,2,3,4,5,6]);
+// var evens = filter(x => ( x % 2 == 0));
+// evens([1,2,3,4,5,6]);
+
+//this reduce does not support partial functions
+var reduce1 = function (comb, acc, list) {
+    if ( list.length == 0 ) {return acc; }
+    else {	
+	va1r [h, ...t] = list;
+	return reduce (comb , comb(acc,h), t );		      
+    }
+};
+// reduce1 ((x, y) => x + y, 0, [1,2,3]);;
+// var sum1 = (list => (reduce1((x, y) => x + y, 0, list)));
+
+
+//this reduce supports one step partial functiona application on list
+var reduce = function (comb, acc, list) {
+    if (typeof list == "undefined") { //partial
+	return function (list) {
+	    if ( list.length == 0 ) {return acc; }
+	    else {
+		var [h, ...t] = list;
+		return reduce (comb , comb(acc,h), t );
+	    }
+	};
+    }
+    //full application
+    if ( list.length == 0 ) {return acc; }
+    else {	
+	var [h, ...t] = list;
+	return reduce (comb , comb(acc,h), t );		      
+    }
+};
+//reduce ((x, y) => x + y, 0, [1,2,3]);;
+//var sum = reduce ((x, y) => x + y, 0);
+//sum([1,2,3]);
+//var concat = reduce ((x, y)=> x + y, "");
+//concat(["hi ", "there ", "jimmy"]);
+
+
+
+
+
+
+
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+    
