bc52d0b2fe8232fb0a78d43c292bd589383fa9f6
sach <sacha.saint-leger@mail.mcgill.ca>
2015-12-27 13:42:36 -0500

    added one step partial function application to map and reduce

--- helpers.jsx
+++ helpers.jsx
@@ -2,6 +2,25 @@
 
 var map = function (transform, items) {
 
+    if (typeof items == "undefined") { //partial
+	return function (items) {
+	    var acc = [];
+	    if ( items.length == 0) { return [] ;}
+	    function aux (items, transform, acc) {
+		var [h, ...t] = items;
+
+         	if (t.length == 0) {
+		    acc.push(transform(h));
+	 	    return acc; }
+	        else {
+		    acc.push(transform(h));
+		    return aux(t, transform, acc);
+		}
+	    }
+	    return aux (items, transform, acc);
+	};
+    }
+    // full
     var acc = [];
     if ( items.length == 0) { return [] ;}
 
@@ -20,7 +39,9 @@ var map = function (transform, items) {
 
     return aux (items, transform, acc);
 };
-// map ([1,2,3], x => x + 1);
+// map (x => x + 1, [1,2,3]);
+// var add_one = map(x => x + 1);
+// add_one([1,2,3]);
 
 var filter = function (pred, list) {
     var acc = [];
@@ -45,16 +66,39 @@ var filter = function (pred, list) {
 // filter([1,2,3,4,6], x => ( x % 2 == 0));	    
 
 
-var reduce = function (comb, acc, list) {
+//this reduce does not support partial functions
+var reduce1 = function (comb, acc, list) {
     if ( list.length == 0 ) {return acc; }
     else {	
 	var [h, ...t] = list;
 	return reduce (comb , comb(acc,h), t );		      
     }
 };
-// reduce ((x, y) => x + y, 0, [1,2,3]);;
+// reduce1 ((x, y) => x + y, 0, [1,2,3]);;
 
+
+//this reduce supports one step currying on list
+var reduce = function (comb, acc, list) {
+    if (typeof list == "undefined") { //partial
+	return function (list) {
+	    if ( list.length == 0 ) {return acc; }
+	    else {
+		var [h, ...t] = list;
+		return reduce (comb , comb(acc,h), t );
+	    }
+	};
+    }
+    //full application
+    if ( list.length == 0 ) {return acc; }
+    else {	
+	var [h, ...t] = list;
+	return reduce (comb , comb(acc,h), t );		      
+    }
+};
 //var sum = reduce ((x, y) => x + y, 0);
+//sum([1,2,3]);
+
+
 
 
     
